955e7f4d9e9c7fce2e4edfd5f1b336770c9503e4
Tagir Askarov <askarovtagir1@gmail.com>
2024-08-21 11:00:24 +0300

    надеюс все

---
+++ src/main/java/ru/kata/spring/boot_security/demo/services/UserService.java
@@ -0,0 +1,15 @@
+package ru.kata.spring.boot_security.demo.services;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+import ru.kata.spring.boot_security.demo.entity.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserService extends UserDetailsService {
+    User findByUsername(String username);
+    void saveUser(User user);
+    List<User> getAllUsers();
+    Optional<User> getUser(long id);
+    void deleteUser(long id);
+}
